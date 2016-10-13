7534da7bdafd7559f9f63a77c273934e6307ca7a
taka yaeo <taka-yaeo@yaeo.local>
2016-10-13 16:06:41 +0900

    add create action to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create
+    Message.create(create_message_params)
+    redirect_to root_path
+  end
+
+  private
+    def create_message_params
+      params.require(:message).permit(:body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'groups#index'
+  resources 'messages'
 end
