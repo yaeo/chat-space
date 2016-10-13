1f598a395317dbaa91bf816475265a15059d0cb1
taka yaeo <taka-yaeo@yaeo.local>
2016-10-13 18:01:09 +0900

    modify create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,14 @@
 class MessagesController < ApplicationController
   def create
-    @message = Message.create(create_message_params)
-    flash[:error] = @message.errors.full_messages
-    redirect_to root_path
+    @message = Message.new(create_message_params)
+    if @message.save
+      #メッセージの保存に成功したときの処理
+      redirect_to root_path
+    else
+      #メッセージの保存に失敗したときの処理
+      flash[:error] = @message.errors.full_messages
+      redirect_to root_path
+    end
   end
 
   private
