e07724476ebd58a319ae8ed122f6df979d04a3dd
taka yaeo <taka-yaeo@yaeo.local>
2016-10-14 12:08:31 +0900

    finish test for groups controller

---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe GroupsController do
+  describe 'GET #index' do
+    #indexテンプレートが表示されるかどうか
+    it 'render the :index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+    #新しいMessageインスタンスが作成されているかどうか
+    it 'assigns a new message to @message' do
+      get :index
+      expect(assigns(:message)).to be_a_new(Message)
+    end
+  end
+end
