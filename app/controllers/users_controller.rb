e4d82e52b85c786c2f42348b7a10ec8e2d812659
taka yaeo <taka-yaeo@yaeo.local>
2016-10-14 21:13:34 +0900

    finish user edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(current_user)
   end
+
+  def update
+    current_user.update_with_password(update_user_params)
+    redirect_to root_path
+  end
+
+  private
+    def update_user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
 end
