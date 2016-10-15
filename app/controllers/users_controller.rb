da58217551c901f43f8c632141d0658b5084de6f
taka yaeo <taka-yaeo@yaeo.local>
2016-10-15 15:25:30 +0900

    Change private method name and edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
   def edit
-    @user = User.find(current_user)
   end
 
   def update
-    current_user.update_with_password(update_user_params)
+    current_user.update_with_password(user_params)
     redirect_to root_path
   end
 
   private
-    def update_user_params
+    def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
     end
 end
