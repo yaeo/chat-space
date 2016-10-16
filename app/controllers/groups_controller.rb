2ccda38e84ab6ef9459e83c896933a5dcc6b143d
taka yaeo <taka-yaeo@yaeo.local>
2016-10-16 17:04:34 +0900

    Create actions in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,20 @@ class GroupsController < ApplicationController
   def index
     @message = Message.new
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
