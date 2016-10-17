76787f20f1561a1afbe8ce0146e9baf71b9c69df
taka yaeo <taka-yaeo@yaeo.local>
2016-10-17 21:09:42 +0900

    Add hidden_field for group_id to save

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
 
   private
     def message_params
-      #todo group_idを選択しているグループのものを入れるようにする
-      params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: "1")
+      #todo hidden fieldでいまいるgroup_idを入れる
+      params.require(:message).permit(:body, :group_id).merge(user_id: current_user.id)
     end
 end
