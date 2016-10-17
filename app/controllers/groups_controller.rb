beddf963e5f5133060a4a76c9a9901bb8d6f1b40
taka yaeo <taka-yaeo@yaeo.local>
2016-10-17 13:10:33 +0900

    Able to save member at same time group is created

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,8 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
+      member = Member.new(group_id: @group.id, user_id: params[:group][:user_ids][0])
+      member.save
       redirect_to root_url
     else
       #失敗したとき
@@ -28,6 +30,10 @@ class GroupsController < ApplicationController
 
     def group_params
       params.require(:group).permit(:name)
+    end
+
+    def member_params
+      params.require(:group).permit(:user_ids)
     end
 
 end
