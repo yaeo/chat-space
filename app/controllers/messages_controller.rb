4f45404200740a96aab43e3eaab36b0a4f6c8b13
taka yaeo <taka-yaeo@yaeo.local>
2016-10-17 21:12:57 +0900

    Change redirect url to self.group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       #メッセージの保存に成功したときの処理
-      redirect_to root_path
+      redirect_to group_url(params[:message][:group_id])
     else
       #メッセージの保存に失敗したときの処理
       flash[:errors] = @message.errors.full_messages
@@ -13,7 +13,6 @@ class MessagesController < ApplicationController
 
   private
     def message_params
-      #todo hidden fieldでいまいるgroup_idを入れる
       params.require(:message).permit(:body, :group_id).merge(user_id: current_user.id)
     end
 end
