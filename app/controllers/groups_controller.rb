6027f42437ed4456d3b58ce25c4a4e1698634117
taka yaeo <taka-yaeo@yaeo.local>
2016-10-17 18:23:52 +0900

    Create members record when group is made

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,15 +7,10 @@ class GroupsController < ApplicationController
     # users = User.all
     @group = Group.new
     @group.members.build
-    @group.users.build
   end
 
   def create
     @group = Group.create(group_params)
-    # @group = Group.new(name: params[:group][:name], members_attributes: [ {user_id: params[:group][:user_ids][1]},{user_id: params[:group][:user_ids][2]} ])
-    # if @group.save
-      # member = Member.new(group_id: @group.id, user_id: params[:group][:user_ids][0])
-      # member.save
     redirect_to root_url
     # else
     #   #失敗したとき
@@ -33,6 +28,7 @@ class GroupsController < ApplicationController
   private
 
     def group_params
-       params.require(:group).permit(:name, members_attributes:[:user_id])
+       params.require(:group).permit(:name, :user_ids => [])
+      #  binding.pry
     end
 end
