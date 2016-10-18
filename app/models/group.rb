ce84487f4dd7abf29130db8886797c02ae223b2a
taka yaeo <taka-yaeo@yaeo.local>
2016-10-18 20:12:11 +0900

    Remove unnecessary association option

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   #association
-  has_many :users, through: :members, source: :user
+  has_many :users, through: :members
   has_many :messages
   has_many :members
   accepts_nested_attributes_for :members
