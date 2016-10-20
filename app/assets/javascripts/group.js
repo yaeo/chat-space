3ce917d0a8c66334815ab5032b18fb3f71294566
taka yaeo <taka-yaeo@yaeo.local>
2016-10-20 16:03:55 +0900

    Add delete member button

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,6 +3,6 @@ $(function() {
     e.preventDefault();
     var userId = $(this).data('userId');
     var userName = $(this).data('userName');
-    $('#group-user').append("<div class= 'chat-group-user clearfix'><input multiple='multiple' type='hidden' name='group[user_ids][]' value="+userId+ "><p class='chat-group-user__name'>"+userName+"</div>");
+    $('#group-user').append("<div class= 'chat-group-user clearfix'><input multiple='multiple' type='hidden' name='group[user_ids][]', value="+userId+ "><p class='chat-group-user__name'>"+userName+"</p><a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove', data-user-id="+userId+">削除</a></div>");
   });
 });
