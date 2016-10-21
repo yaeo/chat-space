535e24b2b8a6d412b767571fa89be239b024acf9
taka yaeo <taka-yaeo@yaeo.local>
2016-10-21 14:02:55 +0900

    Modify group edit page

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -2,6 +2,7 @@
 $(function() {
   $('#user-search-result').on('click', '.user-search-add', function(e) {
     e.preventDefault();
+    $(this).parent().remove();
     var userId = $(this).data('user_id');
     var userName = $(this).data('user_name');
     $('#group-user').append("<div class= 'chat-group-user clearfix'><input multiple='multiple' type='hidden' name='group[user_ids][]', value="+userId+ "><p class='chat-group-user__name'>"+userName+"</p><a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove', data-user-id="+userId+">削除</a></div>");
