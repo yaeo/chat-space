05b01c10ef8811820d66955bb38a8ce5466732e4
taka yaeo <taka-yaeo@yaeo.local>
2016-10-21 14:32:51 +0900

    Modify js file

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -23,13 +23,19 @@ $(function() {
   var preWord;
   // 検索ユーザーを結果欄に表示させる
   function appendList(user) {
-    var item = $("<div class= 'chat-group-user clearfix'>").append("<p class='chat-group-user__name'>"+user.name+"</p><a class='user-search-add chat-group-user__btn chat-group-user__btn--add', data-user_id='"+user.id+"', data-user_name='"+user.name+"'>追加</a>");
+    var item = $("<div class= 'chat-group-user clearfix'>")
+               .append("<p class='chat-group-user__name'>"+user.name+"</p> \
+               <a \
+               class='user-search-add chat-group-user__btn chat-group-user__btn--add' \
+               data-user_id='"+user.id+"' \
+               data-user_name='"+user.name+"'> \
+               追加</a>");
     list.append(item);
   }
   // json検索ワードを送信して、候補ユーザーを受け取る
   $('#keyword_keyword').on("keyup", function() {
       $("#user-search-result").empty();
-      var input = $('#keyword_keyword').val();
+      var input = $(this).val();
         if(input !== preWord) {
           $.ajax({
             url: 'http://localhost:3000/users.json',
