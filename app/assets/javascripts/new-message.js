bf1392ff17004237ee55ba0e5d7005dd4566801e
taka yaeo <taka-yaeo@yaeo.local>
2016-10-21 17:44:37 +0900

    Able to send image file by ajax

--- app/assets/javascripts/new-message.js
+++ app/assets/javascripts/new-message.js
@@ -1,22 +1,35 @@
 // jsonで飛んできたメッセージのhtmlを生成して追加する
 function insertHtml(data) {
-  var html = "<div class= 'message__header clearfix'><div class='message__header__name'>"+data.name+"</div><div class='message__header__date'>"+data.date+"</div></div><div class='message__body'>"+data.body+"</div>";
+  var html = "<div class= 'message__header clearfix'>\
+                <div class='message__header__name'>"+data.name+"</div>\
+                <div class='message__header__date'>"+data.date+"</div>\
+              </div>\
+              <div class='message__body'>\
+                <p>"+data.body+"</p>\
+                <img src="+data.image.url+"/>\
+              </div>";
+
   $('#ajax').append(html);
 };
 
 $(function() {
   $('#message-form').on('submit', function(e) {
       e.preventDefault();
+      var form = $('#message-form').get()[0];
+
+      var formData = new FormData(form);
+
+      // ajaxで送信
       $.ajax({
         url: 'http://localhost:3000/messages.json',
         type: 'POST',
         datatype: 'json',
-        data: {
-          message: {
-            body: $(this).find('#message_body').prop('value'),
-            group_id: $(this).find('#message_group_id').prop('value'),
-          }
-        }
+        data:  formData,
+            // body: $(this).find('#message_body').prop('value'),
+            // image: $(this).find('#message_image').prop('value'),
+            // group_id: $(this).find('#message_group_id').prop('value'),
+        processData: false,
+        contentType: false
       })
       .done(function(data) {
         insertHtml(data);
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,11 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         format.html { redirect_to group_url(current_group)}
-        format.json { render json: {
-          name: @message.user.name,
-          date: @message.created_at.strftime('%Y年%m月%d日 %H:%M'),
-          body: @message.body, }
+        format.json { render json: @message
+          # name: @message.user.name,
+          # date: @message.created_at.strftime('%Y年%m月%d日 %H:%M'),
+          # body: @message.body,
+          # image: @message.image
         }
       else
         flash[:errors] = @message.errors.full_messages
@@ -19,7 +20,6 @@ class MessagesController < ApplicationController
 
   private
     def message_params
-      binding.pry
       params.require(:message).permit(:body, :group_id, :image).merge(user_id: current_user.id)
     end
 end
