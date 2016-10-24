5a5b845d2f442e7bdfd0d8f2b0044832b1ab2e37
taka yaeo <taka-yaeo@yaeo.local>
2016-10-24 16:29:21 +0900

    Setup capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
+server '00.00.000.000', user: 'yaeo', roles: %w{app db web}
 
+set :ssh_options, keys: '~/.ssh/ec2_key_rsa'
 
 
 # role-based syntax
